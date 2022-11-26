716a8c799dd01e6db1fcd687c7be706cdf570250
Enamulhaque0e <enamulhaq243@gmail.com>
2022-11-26 21:31:14 +0600

    firebase environment variables

--- src/Firebase/Firebase.Config.js
+++ src/Firebase/Firebase.Config.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzZzC7o3m6ZEgSy2kOXpbjLP2_LfYJ7ZI",
-  authDomain: "sell-cell-7d7b6.firebaseapp.com",
-  projectId: "sell-cell-7d7b6",
-  storageBucket: "sell-cell-7d7b6.appspot.com",
-  messagingSenderId: "1020951763658",
-  appId: "1:1020951763658:web:8d68855ae2976fbad27233"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
+
+
 
 
 
